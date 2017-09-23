d64edd484cfd2bb3c6d9f0577651cbe15a2b5edb
Lars Weje Hangstrup <lars@hangstrup.dk>
2017-09-23 23:36:14 +0200

    Some refactoring

---
+++ README.md
@@ -0,0 +1,9 @@
+# arduinoAlarmState
+
+**Platform**
+The software is running on an Arduino Nano with ENC28J60 Ethernet Shield.
+
+**Purpose**
+The software is used to monitor the status of an Chuango (in Denmark: 'Sikkerthjem') alarm.
+
+For more information see this instructable: 
--- arduinoAlarmMonitor.ino
+++ arduinoAlarmMonitor.ino
@@ -15,10 +15,9 @@
 #include <UIPEthernet.h> // Used for Nano/Shield
 
 // **** ETHERNET SETTING ****
-//#define ALARM_NANO 0x02
-#define ALARM_UNO  0x03
+#define ALARM_ID  0x03  // When using UIPEthernet.h (= not Ethernet.h) this value is reflected in the DHCP naming: ENC28JBE0003 (you can run 'ping ENC28JBE0003')
 
-byte mac[] = { 0xCA, 0xFE, 0xBA, 0xBE, 0x00, ALARM_UNO };
+byte mac[] = { 0xCA, 0xFE, 0xBA, 0xBE, 0x00, ALARM_ID };
 int port = 12900;
 
 EthernetServer server(port);
@@ -31,7 +30,6 @@ typedef enum StateTypeTag {
 } StateType;
 
 StateType state = STATE_NULL;
-//StateType previousDetect = state;
 
 String stateString = "notInitialized";
 
@@ -48,58 +46,55 @@ void setup() {
 
 	Serial.begin(9600);
 
-	#define IN_ALARM	17 //A3
-	#define OUT_RED     16 //A2
-	#define OUT_YELLOW  15 //A1
-	#define OUT_GREEN   14 //A0
+	#define IN_ALARM	17 // = A3
+	#define OUT_RED     16 // = A2
+	#define OUT_YELLOW  15 // = A1
+	#define OUT_GREEN   14 // = A0
 
 	#define IN_ARMED    7
  	#define IN_SHELL	6
 	#define IN_DISABLED 5
 	#define OUT_Z1      4
 	#define OUT_Z2      3
-	#define IN_PUSH		9	// #2 seems reserved (for ENC28J60 based ethernet shield?)
+	#define IN_PUSH		9	// pin #2 seems reserved (for ENC28J60 based ethernet shield?)
 
 
-  pinMode(IN_ALARM, INPUT);
-  pinMode(OUT_RED, OUTPUT);
-  pinMode(OUT_YELLOW, OUTPUT);
-  pinMode(OUT_GREEN, OUTPUT);
+	pinMode(IN_ALARM, INPUT);
+	pinMode(OUT_RED, OUTPUT);
+	pinMode(OUT_YELLOW, OUTPUT);
+	pinMode(OUT_GREEN, OUTPUT);
 
-  pinMode(IN_ARMED, INPUT);
-  pinMode(IN_SHELL, INPUT);
-  pinMode(IN_DISABLED, INPUT);
-  pinMode(OUT_Z1, OUTPUT);
-  pinMode(OUT_Z2, OUTPUT);
-  pinMode(IN_PUSH, INPUT);
+	pinMode(IN_ARMED, INPUT);
+	pinMode(IN_SHELL, INPUT);
+	pinMode(IN_DISABLED, INPUT);
+	pinMode(OUT_Z1, OUTPUT);
+	pinMode(OUT_Z2, OUTPUT);
+	pinMode(IN_PUSH, INPUT);
 
-  digitalWrite(OUT_RED, HIGH);   // Light up, to show that we are starting
-  digitalWrite(OUT_YELLOW, HIGH);// Light up, to show that we are starting
-  digitalWrite(OUT_GREEN, HIGH); // Light up, to show that we are starting
-  digitalWrite(OUT_Z1, HIGH);    // Pulse HIGH -> LOW -> HIGH triggers alarm on Z1 even if disabled. 220R series resistor is OK. 1K8 is too much.
-  digitalWrite(OUT_Z2, HIGH);    // Pulse HIGH -> LOW -> HIGH triggers alarm on Z2 if shell is enabled
+	digitalWrite(OUT_RED, HIGH);   // Light up, to show that we are starting
+	digitalWrite(OUT_YELLOW, HIGH);// Light up, to show that we are starting
+	digitalWrite(OUT_GREEN, HIGH); // Light up, to show that we are starting
+	digitalWrite(OUT_Z1, HIGH);    // Pulse HIGH -> LOW -> HIGH triggers alarm on Z1 even if disabled. 220R series resistor is OK. 1K8 is too much.
+	digitalWrite(OUT_Z2, HIGH);    // Pulse HIGH -> LOW -> HIGH triggers alarm on Z2 if shell is enabled
 
-  // Turn on pull up
+	// Turn on pull up
 #define ALARM_ANALOG
-#ifndef ALARM_ANALOG
-  digitalWrite(IN_ALARM, HIGH);
-#endif
-  digitalWrite(IN_ARMED, HIGH);
-  digitalWrite(IN_SHELL, HIGH);
-  digitalWrite(IN_DISABLED, HIGH);
-  digitalWrite(IN_PUSH, HIGH);  // Pulls low for 3s when alarm is disabled (to allow a push-door to open)
+	digitalWrite(IN_ARMED, HIGH);
+	digitalWrite(IN_SHELL, HIGH);
+	digitalWrite(IN_DISABLED, HIGH);
+	digitalWrite(IN_PUSH, HIGH);  // Pulls low for 3s when alarm is disabled (to allow a push-door to open)
 
 
-  // start the Ethernet connection and the server:
+	// start the Ethernet connection and the server:
 #ifndef UIPETHERNET_H
-  Ethernet.hostName((char *)"Alar2");
-  // When UIPETHERNET is used, the name of the platform is ENC28JBE + macLast4digits, eg.: ENC28JBE0003
+	Ethernet.hostName((char *)"Alar2");
+	// When UIPETHERNET is used, the name of the platform is ENC28JBE + macLast4digits, eg.: ENC28JBE0003
 #endif
-  Ethernet.begin(mac);
-  server.begin();
+	Ethernet.begin(mac);
+	server.begin();
 
-  Serial.print("IP Address: ");
-  Serial.println(Ethernet.localIP());
+	Serial.print("IP Address: ");
+	Serial.println(Ethernet.localIP());
 }
 
 void lightUp() {
@@ -158,31 +153,7 @@ void detectState() {
 	}
 
 	state = thisDetect;
-//	static int debounce = 0;
-//	if (previousDetect != thisDetect) {
-//		Serial.println("Debounce = 0");
-//		debounce=0;
-//	} else {
-//		if (debounce < 5) {
-//			Serial.println("Debounce++");
-//			debounce++;
-//		} else {
-//			state = thisDetect;
-//			Serial.println("State SET!");
-//		}
-//	}
-//	previousDetect = thisDetect;
 
-#ifndef ALARM_ANALOG
-	if (digitalRead(IN_ALARM)) {
-		// Alarm state is inverted through BC547 transistor
-		alarmState = ALARM_STATE_NULL;
-		alarmStateString = "alarm_state_null";
-	} else {
-		alarmState = ALARM_STATE_ACTIVE;
-		alarmStateString = "alarm_state_active";
-	}
-#endif
 }
 
 EthernetClient client;
@@ -244,7 +215,6 @@ void communicate() {
 
 		// close the connection:
 		client.stop();
-		//    Serial.println("   Disconnected\n");
 	}
 }
 
@@ -252,23 +222,19 @@ void returnInputStatus() {
 	int armed = digitalRead(IN_ARMED);
 	int shell = digitalRead(IN_SHELL);
 	int disa = digitalRead(IN_DISABLED);
-#ifdef ALARM_ANALOG
 	int alarm = analogRead(IN_ALARM);
-#else
-	int alarm = digitalRead(IN_ALARM);
-#endif
 	int door = digitalRead(IN_PUSH);
 
 	char str[50];
-	sprintf(str,  "armed:%d shell:%d disa:%d alarm:%d door:%d", armed, shell, disa, alarm, door);
+	sprintf(str,  "armed:%d shell:%d disa:%d alarm(analog value):%d door:%d", armed, shell, disa, alarm, door);
 	client.println(str);
 	Serial.println(str);
 }
 
 void pulsePin(int pin) {
-	  digitalWrite(pin, LOW);    // Pulse HIGH -> LOW -> HIGH triggers alarm on Z1 even if disabled. 220R series resistor is OK. 1K8 is too much.
-	  delay(1000);
-	  digitalWrite(pin, HIGH);
+	digitalWrite(pin, LOW);    // Pulse HIGH -> LOW -> HIGH triggers alarm on Z1 even if disabled. 220R series resistor is OK. 1K8 is too much.
+	delay(1000);
+	digitalWrite(pin, HIGH);
 }
 
 void loop() {
